fa36d6b7522b6cdafe51f650ecf4a59c2a99b67d
frank@gmail.com <imafidonfrank2015@gmail.com>
2025-04-24 04:41:25 +0100

    first commit

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -177,12 +177,12 @@ const Audio = ({mobile}) => {
                       const res = await response.json();
                       audioURL = res.url.toString()
                       uploaded_audio = audioURL
-                      emailjs.send("service_k0d80hp","template_mp8ld0f",{
+                      emailjs.send("service_wo28vkf","template_2odlhkh",{
                         to_name: usertype,
                         message: `${uploaded_audio}`,
                         to_email: usertype,
                         from_email: filesender
-                    },  {publicKey: 'A3D4HSPHNJ8f_odij'});
+                    },  {publicKey: '76FU_4OL25685iLZx'});
                       //await uploadAudio({fileowner, uploaded_audio, filesender}).unwrap()
                       setloading(false)
                       dispatch({type: 'open', size: 'mini'})
